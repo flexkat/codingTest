367fef45c5cc50f65ba71644796a39b18eb0c435
flexkat <catherinemaritz1@gmail.com>
2020-12-03 15:42:34 +1300

    reassembles array of strings into one string

--- stringAssembler.js
+++ stringAssembler.js
@@ -1,8 +1,8 @@
 
-let s1 = "all is well";
-let s2 = "ell that en";
-let s3 = "hat end";
-let s4 = "t ends well";
+let s3 = "all is well";
+let s4 = "ell that en";
+let s1 = "hat end";
+let s2 = "t ends well";
 
 const mergeStrings = (string1, string2) => {
   let mergedString = "";
@@ -37,21 +37,27 @@ const mergeStrings = (string1, string2) => {
 const checkOverlap = (string1, string2) => {
   let overlapMax = 0;
 
-  if (string1.includes(string2)) overlapMax = string2.length;
+  if (!string1 || !string2) {
+    console.log("error, missing a string")
 
-  for (let i = 0; i < string2.length; i++) {
-    let substring = string2.substring(i);
+  } else {
 
-    if (string1.startsWith(substring)) {
-      overlapMax = i;
+    if (string1.includes(string2)) overlapMax = string2.length;
+
+    for (let i = 0; i < string2.length; i++) {
+      let substring = string2.substring(i);
+
+      if (string1.startsWith(substring)) {
+        overlapMax = i;
+      }
     }
-  }
 
-  for (let i = string2.length; i > 0; i--) {
-    let substring = string2.substring(0, i);
+    for (let i = string2.length; i > 0; i--) {
+      let substring = string2.substring(0, i);
 
-    if (string1.endsWith(substring)) {
-      overlapMax = i;
+      if (string1.endsWith(substring)) {
+        overlapMax = i;
+      }
     }
   }
 
@@ -61,13 +67,59 @@ const checkOverlap = (string1, string2) => {
 
 const assembleFragments = (strings) => {
   let mergedString = "";
+  let stringA = "";
+  let stringB = "";
+  let maxOverlapValue = 0;
 
   // for each string pass it through checkOverlap and record overlapMax
   // if overlapMax is the largest value => add that merged string and remove the piece strings
   // start again and compare each string to each other
-  for (let i = 0; i < strings.length; i++) {
-    let { mergedString, overlapMax } = checkOverlap(strings[i], strings[i + 1])
+  for (let i = 0; i < strings.length - 1; i++) {
+    let indexOfSecondString = i + 1
+    // string 1 compares 2,3,4
+    // string 2 compares 3,4
+    // string 3 compares 4
+
+
+
+    while (indexOfSecondString <= strings.length - 1) {
+      let overlapMax = checkOverlap(strings[i], strings[indexOfSecondString])
+
+      if (overlapMax > maxOverlapValue) {
+        maxOverlapValue = overlapMax;
+        stringA = strings[i];
+        stringB = strings[indexOfSecondString];
+      }
+      indexOfSecondString++;
+    }
   }
+
+  mergedString = mergeStrings(stringA, stringB)
+  console.log({ maxOverlapValue, stringA, stringB })
+
+  let newStrings = strings.filter(string => {
+    if (string == stringA || string == stringB) return false
+    return true
+  }
+  )
+
+  newStrings.push(mergedString)
+
+  console.log({ newStrings }, newStrings.length)
+
+  if (newStrings.length == 1) return newStrings[0]
+  else {
+    assembleFragments(newStrings)
+
+  }
+  return newStrings;
 }
 
-export { checkOverlap, assembleFragments, mergeStrings }
+assembleFragments([s1, s2, s3, s4]);
+
+
+
+
+
+
+// export { checkOverlap, assembleFragments, mergeStrings }
