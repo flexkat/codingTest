b414c16276b4cf3b8164c6cc2e8ba98bc5139a53
flexkat <catherinemaritz1@gmail.com>
2020-12-03 13:18:41 +1300

    split checkOverlap into separate chceck and merge functions

--- stringAssembler.js
+++ stringAssembler.js
@@ -4,14 +4,11 @@ let s2 = "ell that en";
 let s3 = "hat end";
 let s4 = "t ends well";
 
-const checkOverlap = (string1, string2) => {
-  let overlapMax = 0;
-  let overlapString = "";
+const mergeStrings = (string1, string2) => {
   let mergedString = "";
 
   if (string1.includes(string2)) {
-    overlapMax = string2.length;
-    overlapString = string2;
+    mergedString = string2;
   }
 
   for (let i = 0; i < string2.length; i++) {
@@ -19,8 +16,6 @@ const checkOverlap = (string1, string2) => {
     let remaining = string2.substring(0, i);
 
     if (string1.startsWith(substring)) {
-      overlapString = substring;
-      overlapMax = i;
       mergedString = remaining + string1;
     }
   }
@@ -30,18 +25,38 @@ const checkOverlap = (string1, string2) => {
     let remaining = string2.substring(i);
 
     if (string1.endsWith(substring)) {
-      overlapString = substring;
-      overlapMax = i;
       mergedString = string1 + remaining;
     }
   }
 
   if (!mergedString) mergedString = string1 + string2;
 
-  console.log({ mergedString, overlapMax })
-  return { mergedString, overlapMax };
+  return mergedString;
 }
 
+const checkOverlap = (string1, string2) => {
+  let overlapMax = 0;
+
+  if (string1.includes(string2)) overlapMax = string2.length;
+
+  for (let i = 0; i < string2.length; i++) {
+    let substring = string2.substring(i);
+
+    if (string1.startsWith(substring)) {
+      overlapMax = i;
+    }
+  }
+
+  for (let i = string2.length; i > 0; i--) {
+    let substring = string2.substring(0, i);
+
+    if (string1.endsWith(substring)) {
+      overlapMax = i;
+    }
+  }
+
+  return overlapMax
+}
 // checkOverlap(s1, s2)
 
 const assembleFragments = (strings) => {
@@ -55,4 +70,4 @@ const assembleFragments = (strings) => {
   }
 }
 
-export { checkOverlap, assembleFragments }
+export { checkOverlap, assembleFragments, mergeStrings }
--- stringAssembler.spec.js
+++ stringAssembler.spec.js
@@ -1,23 +1,28 @@
 import jest from 'jest';
 import { describe } from '@jest/globals';
-import { checkOverlap } from './stringAssembler'
+import { checkOverlap, mergeStrings } from './stringAssembler'
 
 describe('checkOverlap', () => {
+  it('should return a number for number of characters or spaces overlapping between two strings', () => {
+    expect(checkOverlap("abcd", "cdef")).toBe(2)
+  })
+  it('should return length of string if strings are the same', () => {
+    expect(checkOverlap("abcd", "abcd")).toBe(4);
+  })
+  it('should return zero if there is no overlap', () => {
+    expect(checkOverlap("adcd", "efdh")).toBe(0)
+  })
+});
+
+describe('mergeStrings', () => {
   it('should return the first string if string1 and string2 are the same', () => {
-    expect(checkOverlap("abcd", "abcd").mergedString).toBe("abcd");
-  })
-  it('should return an object with keys mergedString and overlapMax', () => {
-    expect(checkOverlap("abcd", "cdef")).toHaveProperty("mergedString")
-    expect(checkOverlap("abcd", "cdef")).toHaveProperty("overlapMax")
-  })
-  it('should return a value of the overlapMax, which is the number of characters or spaces overlapping', () => {
-    expect(checkOverlap("abcd", "cdef").overlapMax).toBe(2)
+    expect(mergeStrings("abcd", "abcd")).toBe("abcd");
   })
   it('should return a mergedString with the strings joined, overlapping any matching character', () => {
-    expect(checkOverlap("all is well", "ell that en").mergedString).toBe("all is well that en")
+    expect(mergeStrings("all is well", "ell that en")).toBe("all is well that en")
   })
   it('should join together two strings if there is no overlap', () => {
-    expect(checkOverlap("adcd", "efdh").mergedString).toBe("adcdefdh")
+    expect(mergeStrings("adcd", "efdh")).toBe("adcdefdh")
   })
 });
 
